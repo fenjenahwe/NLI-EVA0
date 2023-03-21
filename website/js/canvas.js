888f691a9b06bf33cb84bbc8b8a086cbe68dd6b7
fenjenahwe <adriana.bsbmkz@gmail.com>
2023-03-21 17:39:17 +0100

    web

--- website/js/canvas.js
+++ website/js/canvas.js
@@ -8,8 +8,6 @@ var character = null;
 var animations = {};
 var animation = null;
 
-var walkarea = null;
-
 function init()
 {
 	//create the rendering context
@@ -69,11 +67,6 @@ function init()
 	});
 	girl_pivot.addChild( girl_selector );
 
-	walkarea = new WalkArea();
-	walkarea.addRect([-50,0,-30],80,50);
-	walkarea.addRect([-90,0,-10],80,20);
-	walkarea.addRect([-110,0,-30],40,50);
-
 
 	character = girl;
 
@@ -92,13 +85,6 @@ function init()
 	loadAnimation("talking","../avatar/"+avatar+"/talking.skanim");
 	loadAnimation("pointing","../avatar/"+avatar+"/pointing.skanim");
 
-	//load a GLTF for the room
-	// var room = new RD.SceneNode({scaling:40,position:[0,-.01,0]});
-	// room.loadGLTF("./data/room.gltf");
-	// scene.root.addChild( room );
-
-	// var gizmo = new RD.Gizmo();
-	// gizmo.mode = RD.Gizmo.ALL;
 
 	// main loop ***********************
 
@@ -108,25 +94,13 @@ function init()
 		gl.canvas.height = document.body.offsetHeight;
 		gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
 
-		var girlpos = girl_pivot.localToGlobal([0,40,0]);
-		//var campos = girl_pivot.localToGlobal([0,50,0]);
-		var camtarget = girl_pivot.localToGlobal([0,50,70]);
-		var smoothtarget = vec3.lerp( vec3.create(), camera.target, camtarget, 0.02 );
-
 		camera.perspective( 60, gl.canvas.width / gl.canvas.height, 0.1, 1000 );
-		// camera.position(10,10,10);
-		// camera.lookAt( camera.position, girlpos, [0,1,0] );
 
 		//clear
 		renderer.clear(bg_color);
 		//render scene
 		renderer.render(scene, camera, null, 0b11 );
 
-		var vertices = walkarea.getVertices();
-		renderer.renderPoints( vertices, null, camera, null,null,null,gl.LINES );
-
-		//gizmo.setTargets([monkey]);
-		//renderer.render( scene, camera, [gizmo] ); //render gizmo on top
 	}
 
 	//main update
@@ -183,7 +157,7 @@ function init()
 
 	context.onmouse = function(e)
 	{
-		//gizmo.onMouse(e);
+
 	}
 
 	//detect clicks
@@ -191,16 +165,7 @@ function init()
 	{
 		if(e.click_time < 200) //fast click
 		{
-			//compute collision with scene
-			// var ray = camera.getRay(e.canvasx, e.canvasy);
-			// var node = scene.testRay( ray, null, 10000, 0b1000 );
-			// console.log(node);
-			/*
-			if( ray.testPlane( RD.ZERO, RD.UP ) ) //collision with infinite plane
-			{
-				console.log( "floor position clicked", ray.collision_point );
-			}
-			*/
+
 		}
 	}
 
@@ -208,19 +173,13 @@ function init()
 	{
 		if(e.dragging)
 		{
-			//orbit camera around
-			//camera.orbit( e.deltax * -0.01, RD.UP );
-			//camera.position = vec3.scaleAndAdd( camera.position, camera.position, RD.UP, e.deltay );
-			// this line camera.move([-e.deltax*0.1, e.deltay*0.1,0]);
-			//girl_pivot.rotate(e.deltax*-0.003,[0,1,0]);
 
 		}
 	}
 
 	context.onmousewheel = function(e)
 	{
-		//move camera forward
-		// camera.moveLocal([0,0,e.wheel < 0 ? 10 : -10] );
+
 	}
 
 	//capture mouse events
